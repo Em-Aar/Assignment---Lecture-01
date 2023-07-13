952488f9a936950b858b42f3b5022041d52fdd71
Em-Aar <mmrhaq@gmail.com>
2023-07-13 14:35:32 +0500

    update

--- Assignment_Lecture1/Question18/index.js
+++ Assignment_Lecture1/Question18/index.js
@@ -7,15 +7,18 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
-var places = ['Makkah', 'Turkey', 'Madina', 'USA', 'Europe'];
-console.log(places);
-console.log(__spreadArray([], places, true).sort());
-console.log(places);
-console.log(__spreadArray([], places, true).sort().reverse());
-console.log(places);
-console.log(places.reverse());
-console.log(places);
-console.log(places.reverse());
-console.log(places);
-console.log(places.sort());
-console.log(places.reverse());
+//Question - 18, Seeing the world
+var places = ['Makkah', 'Turkey', 'Madina', 'USA', 'Europe']; // storing the locations in an array.
+console.log(places); // printing array in its original order.
+console.log('\n', __spreadArray([], places, true).sort()); // printing array in alphabatic order without changing the original list.
+console.log('\n', places); // check the array is in original order. 
+console.log('\n', __spreadArray([], places, true).sort().reverse()); // reverse alphabatic order without changing original order of array
+console.log('\n', places); // check the array is in original order. 
+console.log('\n', places.reverse()); // Reverse the order of your list. 
+console.log('\n', places); // Print the array to show that its order has changed.
+console.log('\n', places.reverse()); // Reverse the order of your list again.
+console.log('\n', places); // Print the array to show that its back in its original order.
+console.log('\n', places.sort()); // Sort your array so it’s stored in alphabetical order.
+console.log('\n', places); // Print the array to show that its order has been changed.
+console.log('\n', places.reverse()); // Sort to change your array so it’s stored in reverse alphabetical order.
+console.log('\n', places); // Print the list to show that its order has changed.
--- Assignment_Lecture1/Question18/index.ts
+++ Assignment_Lecture1/Question18/index.ts
@@ -1,15 +1,16 @@
 //Question - 18, Seeing the world
-var places = ['Makkah','Turkey','Madina','USA','Europe'];
-console.log(places);
-console.log([...places].sort());
-console.log(places);
-console.log([...places].sort().reverse());
-console.log(places);
-console.log(places.reverse());
-console.log(places);
-console.log(places.reverse());
-console.log(places);
-console.log(places.sort());
-console.log(places.reverse());
-
+let places = ['Makkah','Turkey','Madina','USA','Europe'];   // storing the locations in an array.
+console.log(places);                                        // printing array in its original order.
+console.log('\n', [...places].sort());                      // printing array in alphabatic order without changing the original list.
+console.log('\n', places);                                  // check the array is in original order. 
+console.log('\n', [...places].sort().reverse());            // reverse alphabatic order without changing original order of array
+console.log('\n', places);                                  // check the array is in original order. 
+console.log('\n', places.reverse());                        // Reverse the order of your list. 
+console.log('\n', places);                                  // Print the array to show that its order has changed.
+console.log('\n', places.reverse());                        // Reverse the order of your list again.
+console.log('\n', places);                                  // Print the array to show that its back in its original order.
+console.log('\n', places.sort());                           // Sort your array so it’s stored in alphabetical order.
+console.log('\n', places);                                  // Print the array to show that its order has been changed.
+console.log('\n', places.reverse());                        // Sort to change your array so it’s stored in reverse alphabetical order.
+console.log('\n', places);                                  // Print the list to show that its order has changed.
 

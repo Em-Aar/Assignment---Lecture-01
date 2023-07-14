301a38f584933f15a862e66480b206af7b102bcd
Em-Aar <mmrhaq@gmail.com>
2023-07-14 08:37:18 +0500

    update

--- Assignment_Lecture1/Question36/index.js
+++ Assignment_Lecture1/Question36/index.js
@@ -1,3 +1,6 @@
+/*Question - 36, T-Shirt: Write a function called make_shirt() that accepts
+a size and the text of a message that should be printed on the shirt.
+The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function. */
 function make_shirt(size, message) {
     console.log("This t-shirt is ".concat(size, ". '").concat(message, "' is printed on it."));
 }
--- Assignment_Lecture1/Question36/index.ts
+++ Assignment_Lecture1/Question36/index.ts
@@ -1,6 +1,10 @@
-//Question - 36, T-shirts
-function make_shirt(size: string, message: string) {
-    console.log(`This t-shirt is ${size}. '${message}' is printed on it.`)
+/*Question - 36, T-Shirt: Write a function called make_shirt() that accepts 
+a size and the text of a message that should be printed on the shirt. 
+The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function. */
+
+
+function make_shirt(size: string, message: string) { //A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+    console.log(`This t-shirt is ${size}. '${message}' is printed on it.`);
 }
 
 make_shirt ("Medium", "Be Humble, you'll conquer the hearts!");
